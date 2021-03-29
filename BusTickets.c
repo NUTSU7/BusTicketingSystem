e7928baa177b561f4416a16a561ce19d50c1a1d1
NUTSU7 <bivolliviu62@gmail.com>
2021-03-29 20:27:01 +0300

    redone the string to array

--- BusTickets.c
+++ BusTickets.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+// Functions
 void BusList();
 void BusFile();
 void CancelT();
@@ -13,23 +14,21 @@ void Book();
 void Register();
 void nameT();
 void LogIn();
-void SplitNameNumber();
+void SplitNames();
 
-char fs[80];
+// Variables
 const char login[] = "user", pass[] = "pass"; // login verifier
 char login_input[] = "user", pass_input[] = "pass"; // pass
 char list[30][10]={"Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty", "Empty "};
-int fs1;
-int i;
 int dec; // decisions
-char seat[80]; // seat string taken from file
-int seat1[30][80]; //seat after being trasformed into int
+char seat[200]; // seat string taken from file
 char seat2[30][80]; // seat string after being splitted
 char name[200]; // name string taken from file
-char name1[30][10]; // name string splitted into array elements so u can print it
-int tcount; // forgot what this even does
-int cntName=0; // used for spliting the strings
-int cntNumber=0; // same as cntName
+char nameSplit[30][10]; // name string splitted into array elements so u can print it
+int cnt=0; // used for spliting the strings
+int j=0;
+int i;
+int countNames=0;
 
 int main()
 {
@@ -102,8 +101,8 @@ void BusFile()
         fp2 = fopen("name1.txt","r+");
         fgets(name,200,fp2);
         fclose(fp2);
-        SplitNameNumber();
-      //  BusSeats();
+        SplitNames();
+        BusSeats();
         break;
 
         case 2:
@@ -113,7 +112,7 @@ void BusFile()
          fp2 = fopen("name2.txt","r+");
          fgets(name,200,fp2);
          fclose(fp2);
-         SplitNameNumber();
+         SplitNames();
          BusSeats();
          break;
 
@@ -124,7 +123,7 @@ void BusFile()
          fp2 = fopen("name3.txt","r+");
          fgets(name,200,fp2);
          fclose(fp2);
-         SplitNameNumber();
+         SplitNames();
          BusSeats();
          break;
 
@@ -135,7 +134,7 @@ void BusFile()
          fp2 = fopen("name4.txt","r+");
          fgets(name,200,fp2);
          fclose(fp2);
-         SplitNameNumber();
+         SplitNames();
          BusSeats();
          break;
 
@@ -146,56 +145,35 @@ void BusFile()
          fp2 = fopen("name5.txt","r+");
          fgets(name,200,fp2);
          fclose(fp2);
-         SplitNameNumber();
+         SplitNames();
          BusSeats();
          break;
         }
 }
 
-void SplitNameNumber()
+void SplitNames()
 {
-  int j=0;
-  for(int i=0;i<=(strlen(name));i++)
+  for(i=0;i<=(strlen(name));i++)
       {
           // if space or NULL found, assign NULL into splitStrings[cnt]
           if(name[i]==' '||name[i]=='\0')
           {
-              name1[cntName][j]='\0';
-              cntName++;  //for next word
+              nameSplit[cnt][j]='\0';
+              cnt++;  //for next word
               j=0;    //for next word, init index to 0
           }
           else
           {
-              name1[cntName][j]=name[i];
+              nameSplit[cnt][j]=name[i];
               j++;
+              countNames++;
           }
       }
 
-      int k=0;
-      for(int i=0;i<=(strlen(seat));i++)
-      {
-        // if space or NULL found, assign NULL into splitStrings[cnt]
-        if(seat[i]==' '||seat[i]=='\0')
-        {
-          seat2[cntNumber][k]='\0';
-          cntNumber++;  //for next word
-          k=0;    //for next word, init index to 0
-        }
-        else
-        {
-          seat2[cntNumber][k]=seat[i];
-          k++;
-        }
-      }
-
-  //    for(int i=0; i<sizeof(seat2); i++){
-  //      seat1[i] = atoi(seat2[i]);
-  //    }
-
-      for(int i=0; i<cntNumber; i++)
-      {
-        printf(" %s.%s\t", seat2[i], name1[i]);
-      }
+//      for(int i=0; i<cntName; i++)
+//      {
+//        printf("%d.%s\t", i+1, name1[i]);
+//      }
 }
 
 void nameT()
@@ -225,25 +203,15 @@ void BusStatus()
 
 void BusSeats()
 {
-/*    seat1 = atoi(seat);
     for(i=1; i<=30; i++){
-        if(i == seat1){
-            printf(" %s.%s\t", seat, name1[i]);
-            continue;
+        if(i<3){
+            printf("%d.%s", i, nameSplit[i-1]);
         }
         else
             printf(" %d.%s\t", i, list[i-1]); /* to start the array from */
 
-            int num10=sizeof(name1)+1;
-            for(int i=1; i<=2; i++){
-                    printf(" %d.%s\t", i, name1[i-1]);
-                  }
-
-                    for(int k=i; k<=30; k++){
-                    printf(" %d.%s\t", k, list[k-1]); /* to start the array from */
-                  }
-
-    printf("\n\n\nAvailable Seats:%d\n",fs1);
+}
+    printf("\n\n\nAvailable Seats:%d\n",30-j);
 }
 
 void Book()
@@ -253,13 +221,7 @@ void Book()
 
 void Register()
 {
-  /*
-    FILE *fp, *fopen();
-    fp = fopen("seats1.txt","w+");
-    fgets(fs,80,fp);
-    fclose(fp);
-    BusSeats();
-    */
+
 }
 void LogIn() {
   printf("Username:\n");
