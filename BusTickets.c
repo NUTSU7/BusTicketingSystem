8bd55d1341d5f5121c27873f3779733ed844942d
NUTSU7 <bivolliviu62@gmail.com>
2021-04-01 22:49:53 +0300

    Fixed formatting, got a bug with the splitted strings tho
    Signed-off-by: sajidshahriar72543 <sazidshahriar39@gmail.com>

--- BusTickets.c
+++ BusTickets.c
@@ -6,6 +6,7 @@
 // Functions
 void BusList();
 void BusFile();
+void SplitNames();
 void CancelT();
 void BusStatus();
 void BusSeats();
@@ -14,15 +15,12 @@ void Book();
 void Register();
 void nameT();
 void LogIn();
-void SplitNames();
 
 // Variables
 const char login[] = "user", pass[] = "pass"; // login verifier
 char login_input[] = "user", pass_input[] = "pass"; // pass
 char list[32][10]={"Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty ","Empty", "Empty "};
 int dec; // decisions
-char seat[200]; // seat string taken from file
-char seat2[30][80]; // seat string after being splitted
 char name[200]; // name string taken from file
 char nameSplit[32][10]; // name string splitted into array elements so u can print it
 int cnt=0; // used for spliting the strings
@@ -85,7 +83,6 @@ void BusList()
 void BusFile()
 {
     FILE *fp, *fopen();
-    FILE *fp2, *fopen();
 
     printf("1. Cardiff Express\n2. Delfast Express\n3. Derby Express\n4. Chester Expres\n5. Newport Express\n\n\n");
     printf("Enter your choice: ");
@@ -95,56 +92,41 @@ void BusFile()
     switch (dec) {
 
         case 1:
-        fp = fopen("seats1.txt","r+");
-        fgets(seat,80,fp);
+        fp = fopen("name1.txt","r+");
+        fgets(name,200,fp);
         fclose(fp);
-        fp2 = fopen("name1.txt","r+");
-        fgets(name,200,fp2);
-        fclose(fp2);
         SplitNames();
         BusSeats();
         break;
 
         case 2:
-         fp = fopen("seats2.txt","r+");
-         fgets(seat,80,fp);
+         fp = fopen("name2.txt","r+");
+         fgets(name,200,fp);
          fclose(fp);
-         fp2 = fopen("name2.txt","r+");
-         fgets(name,200,fp2);
-         fclose(fp2);
          SplitNames();
          BusSeats();
          break;
 
         case 3:
-         fp = fopen("seats3.txt","r+");
-         fgets(seat,80,fp);
+         fp = fopen("name3.txt","r+");
+         fgets(name,200,fp);
          fclose(fp);
-         fp2 = fopen("name3.txt","r+");
-         fgets(name,200,fp2);
-         fclose(fp2);
          SplitNames();
          BusSeats();
          break;
 
         case 4:
-         fp = fopen("seats4.txt","r+");
-         fgets(seat,80,fp);
+         fp = fopen("name4.txt","r+");
+         fgets(name,200,fp);
          fclose(fp);
-         fp2 = fopen("name4.txt","r+");
-         fgets(name,200,fp2);
-         fclose(fp2);
          SplitNames();
          BusSeats();
          break;
 
         case 5:
-         fp = fopen("seats5.txt","r+");
-         fgets(seat,80,fp);
+         fp = fopen("name5.txt","r+");
+         fgets(name,200,fp);
          fclose(fp);
-         fp2 = fopen("name5.txt","r+");
-         fgets(name,200,fp2);
-         fclose(fp2);
          SplitNames();
          BusSeats();
          break;
@@ -161,12 +143,12 @@ void SplitNames()
               nameSplit[cnt][j]='\0';
               cnt++;  //for next word
               j=0;    //for next word, init index to 0
+              countNames++;
           }
           else
           {
               nameSplit[cnt][j]=name[i];
               j++;
-              countNames++;
           }
       }
 
@@ -203,16 +185,16 @@ void BusStatus()
 
 void BusSeats()
 {
-  int z=0;
-    for(i=1; i<=32; i++){
+      for(i=1; i<=32; i++){
       if(i % 4 == 0){
         printf("%d.%s\n", i, list[i-1]);
       }
-        else if(i<3){ //its i<3 cuz i just want to test if it can print right, there should be countNames so it will know how many times to print names
+        else if(i<=countNames){
             printf("%d.%s\t", i, nameSplit[i-1]);
         }
-        else
+        else{
             printf("%d.%s\t", i, list[i-1]); /* idk */
+          }
 
 }
     printf("\n\n\nAvailable Seats:%d\n",30-j);
